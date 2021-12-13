ecd2cceab262e116dca1a485eefe512c0adde6a3
Pavel web2cap.ru <web2cap@gmail.com>
2021-12-13 23:09:16 +0300

    make registrations use posts.apps.PostsConfig

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -39,6 +39,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'posts.apps.PostsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
