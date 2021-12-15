17fa4967b88f0e88071dca6b4ba5e5bd6affc511
Pavel web2cap.ru <web2cap@gmail.com>
2021-12-15 21:04:32 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+# Главная страница
+def index(request):
+    return HttpResponse('Главная страницы')
+
+
+# Страница со списком мороженого
+def group_posts(request, slug):
+    return HttpResponse(f'Список постов {slug}')
