72fb9efaa38e801d2459e45c88b6fb537a43897d
Pavel web2cap.ru <web2cap@gmail.com>
2021-12-19 23:04:35 +0300

    name, namespaces

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,10 +4,16 @@ from django.shortcuts import render
 def index(request):
     """Главная страница."""
     template = "posts/index.html"
-    return render(request, template)
+    context = {
+        "text": "Это главная страница проекта Yatube",
+    }
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
     """Страница список постов."""
     template = "posts/group_list.html"
-    return render(request, template)
+    context = {
+        "text": "Здесь будет информация о группах проекта Yatube",
+    }
+    return render(request, template, context)
